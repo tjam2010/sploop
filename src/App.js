c610a65a124f293df8fb7ff79080ad7dcf2649a8
tjam2010 <tjam2010@uw.edu>
2020-01-17 08:51:45 -0800

    project basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main>
+    <header>
+      <img className="logo" src="https://image.freepik.com/free-vector/swan-with-water-splash-logo-design_100735-14.jpg"/>
+      <div className="title">
+        ploop
+      </div>
+    </header>
+    <body>
+    
+    </body>
+    <footer>
+      <TextInput onSend={t=>{
+        var msg = document.createElement("div");   
+        msg.className = "message"
+        msg.innerHTML = t;         
+        document.body.appendChild(msg);  }
+      }/>
+    </footer>
+  </main>
+}
+
+function TextInput(props){
+  const [text, setText] = useState('')
+
+  return <div className="text-input">
+    <input value={text}
+      placeholder="Type here..."
+      onChange={e=>setText(e.target.value)}
+    />
+    <button onClick={()=> {
+      props.onSend(text)
+      setText('')
+    }}>
+      &#x2B9D;
+    </button>
+  </div>
 }
 
 export default App;
