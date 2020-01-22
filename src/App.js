e19a023fa2ae82ec622035ab98076767bd93e2f1
tjam2010 <tjam2010@uw.edu>
2020-01-22 12:47:49 -0800

    added username editing and internet messaging

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import './db'
+import NamePicker from './namePicker'
+import { db } from './db';
 
 function App() {
+  const [messages, setMessages] = useState([])
+  const [name, setName] = useState('')
+
+  useEffect(() =>{
+    db.listen({
+      receive: m=> {
+        setMessages(current=>[m,...current])
+    },
+    })
+  }, [])
+
   return <main>
     <header>
-      <img className="logo" src="https://image.freepik.com/free-vector/swan-with-water-splash-logo-design_100735-14.jpg"/>
       <div className="title">
-        ploop
+        <img className="logo" alt="s" src="https://image.freepik.com/free-vector/swan-with-water-splash-logo-design_100735-14.jpg" />
+        <div>ploop</div>
       </div>
+      <NamePicker onSave={setName}/>
     </header>
     <body>
-    
+      <div className="messages">
+        {messages.map((m,i)=>{
+          return <div key={i} className="message-wrap">
+            <div className="message">{m.text}
+              {m.name}
+            </div>
+          </div>
+        })}
+      </div>
     </body>
     <footer>
-      <TextInput onSend={t=>{
-        var msg = document.createElement("div");   
-        msg.className = "message"
-        msg.innerHTML = t;         
-        document.body.appendChild(msg);  }
-      }/>
+    <TextInput onSend={(text) => {
+        db.send({
+          text:text, name:name, ts: new Date()
+        })
+      }} />
     </footer>
   </main>
 }
 
-function TextInput(props){
+function TextInput(props) {
   const [text, setText] = useState('')
 
-  return <div className="text-input">
-    <input value={text}
+  return <div>
+    <input className="text-input" value={text}
       placeholder="Type here..."
-      onChange={e=>setText(e.target.value)}
+      onChange={e => setText(e.target.value)}
+      onKeyPress={e => {
+        if(e.key === "Enter"){
+            if(text){
+              props.onSend(text)
+              setText('')
+            }
+        }
+      }}
     />
-    <button onClick={()=> {
-      props.onSend(text)
-      setText('')
-    }}>
+    <button className="send-button" onClick={() => {
+      if(text){
+        props.onSend(text)
+        setText('')
+      }
+    }} disabled={!text}>
       &#x2B9D;
     </button>
   </div>
---
+++ src/namePicker.js
@@ -0,0 +1,44 @@
+import React, {useState, useRef, useEffect} from 'react'
+import { FiEdit3, FiSave } from "react-icons/fi";
+
+function NamePicker(props) {
+    const [name, setName] = useState('')
+    const [showName, setShowName] = useState(false)
+    const inputEl = useRef(null)
+
+    function save(){
+        setTimeout(()=>{
+        inputEl.current.focus()
+        },0)
+        if(name && !showName){
+            props.onSave(name)
+            localStorage.setItem('name', name)
+        }
+        setShowName(!showName)
+    }
+
+    useEffect(()=>{
+        const n = localStorage.getItem('name')
+        if(n){
+            setName(n)
+            save()
+        }
+    }, [])
+
+    return <div className="edit-username">
+        <input value={name} ref={inputEl}
+            className = "name-input"
+            style={{display: showName ?'none':'flex'}}
+            onChange={e=> setName(e.target.value)}
+            onKeyPress={e=> {
+                if(e.key==='Enter') save()  
+            }}
+        />
+        {showName && <div className="name-wrap">Hello,&nbsp;<div className="name">{name}</div>!</div>}
+        <button className="name-button" onClick={save}>
+            {showName ? <FiEdit3 /> : <FiSave />}
+        </button>
+    </div>
+}
+
+export default NamePicker
